e8b90b288dfdcc5c602268d54d6ff15cb3346314
engrmhk <engrmhk@yahoo.com>
2023-10-10 23:59:26 +0400

    route changes, home

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+//Route::view('/', 'home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
